620ed66f96f98925f7c2ba16a19afd27ef6a4552
hgjohn <103093070+hgjohn@users.noreply.github.com>
2023-01-15 09:31:47 -0800

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-"""Route subdomain to html."""
-
-from flask import render_template
-from app import app
-from models import Medication
-
-@app.route('/')
-def homepage():
-    return render_template('index.html')
